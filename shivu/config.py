8828d202f1912dc9b5da9ced34767d08031462f0
ArfinVoldigoads <kinganos50@gmail.com>
2024-12-31 16:57:52 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,12 +5,12 @@ class Config(object):
     OWNER_ID = "6995317382"
     sudo_users = "6995317382", "5820365080", "5489586647", "6205444949", "5147271956", "158468045", "6686326398", "5147271956", "7177727796", "5008662958", "7127913934", "5843270062", "1495261563", "6708573850", "6489025882", "6752263178", "5157661772", "6353916710" 
     GROUP_ID = -1002046761940
-    TOKEN = "6829947391:AAHqz3aEnQw8TMJ931Qq-4BlLwBLZrH917c"
+    TOKEN = "8094433736:AAH5C0mmL4a3AD6WVPAvfhe_bf_AXNlCX1g"
     mongo_url = "mongodb+srv://Arfin1:Arfin1@cluster0.5axv7.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/8279229551c22fe1c17de.jpg", "https://telegra.ph/file/ddb237bcd33fc09941312.jpg"]
     SUPPORT_CHAT = "gc_animecommunity"
     UPDATE_CHAT = "Take_update"
-    BOT_USERNAME = "Waifus_database_bot"
+    BOT_USERNAME = "Husbando_database_bot"
     CHARA_CHANNEL_ID = "-1002126509901"
     api_id = 21436816
     api_hash = "c269918dddddbc041d536207cab72155"
